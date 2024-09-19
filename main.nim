94c440b8b250513e6c87bd87466c4cd0bef8d12a
LianelLars <lianel.larson@gmail.com>
2024-09-19 10:28:21 +0300

    Complete

--- main.nim
+++ main.nim
@@ -1,8 +1,44 @@
 type
-    # Для каждого объекта и атрибута после названия добавьте звездочку.
     # Код для приюта (Shelter)
+    Staff* = object
+      name*, birthDate*: string
+      uid*: int
+    Pet* = object
+      name*: string
+      age*: int
+    Manager* = object
+      name*, post*: string
+
     Shelter* = object
+      staff*: Staff
+      pet*: Pet
+      manager*: Manager
 
     # Код для школы (School)
+    Director* = object
+      name*: string
+    Teacher* = object
+      name*, class*: string
+    Student* = object
+      name*, class*: string
+
+    School* = object
+      director*: Director
+      teacher*: Teacher
+      student*: Student
 
     # Код для магазина (Shop)
+    ShopManager* = object
+      name*: string
+    ShopStaff* = object
+      name*, birthDate*, post*: string
+    Good* = object
+      title*, endDate*: string
+    Cash* = object
+      number*: int
+
+    Shop* = object
+      manager*: ShopManager
+      staff*: ShopStaff
+      good*: Good
+      cash*: Cash
